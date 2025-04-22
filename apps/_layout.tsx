75d53d958f64cd622e5041abaa19e8785f407cf4
Dusuan <XxDusuanxX@hotmail.com>
2025-04-22 13:52:44 -0700

    Se hizo un proyecto dentro del proyecto, entonces acomodé todo

--- apps/_layout.tsx
+++ apps/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import {PaperProvider } from "react-native-paper";
+import './styles.css'
 export default function RootLayout() {
   return (
     <PaperProvider>
