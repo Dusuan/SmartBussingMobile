c927b766a50036fbb48b612297c096ff43c526a6
rhamses <rhamses09.rot@gmail.com>
2025-04-22 16:26:51 -0700

    mapa duh

--- source/app/index.tsx
+++ source/app/index.tsx
@@ -1,17 +1,27 @@
 import { Text, View } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
+import React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet} from "react-native";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ActivityIndicator animating={true} size="large" />
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style = {styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
+
+      const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        map: {
+          width: "100%",
+          height: "100%",
+        },
+  });
+
