e682b7b4f371a1ecee07ad46287cb22e3b0b6661
AndresSilva23 <andres.sl@cetys.edu.mx>
2025-05-07 13:37:40 -0700

    Textos Contra y Email

--- app/login.tsx
+++ app/login.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput, Button, Divider } from 'react-native-paper';
 
 const Login = () => {
+
+    const [correo, setCorreo] = React.useState('');
+    const [contraseña, setContraseña] = React.useState('');
+
     return (
-        <View >
-            <Text >Login</Text>
+        <View className='mt-5'>
+            <Text>Correo electrónico</Text>
+        <View className="ml-10 mr-10">
+        <TextInput
+            label="Escriba aqui..."
+            mode="flat"
+            value={correo}
+            onChangeText={correo => setCorreo(correo)}
+        />
         </View>
+        <View className="mt-5">
+            <Text>Contraseña</Text>
+            <View className="ml-10 mr-10">
+        <TextInput
+            label="Escriba aqui..."
+            mode="flat"
+            value={contraseña}
+            onChangeText={contraseña => setContraseña(contraseña)}
+        />
+        </View>
+        </View>
+
+        <View className="mt-10 ml-20 mr-20">
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+            Iniciar sesión
+        </Button>
+        </View>
+     </View>
     );
 };
 
