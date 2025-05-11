af8fd87c0dd977208d901fd423c86434cbdf3f0c
Pepino8 <cristobalcoronador@gmail.com>
2025-05-10 22:25:51 -0700

    Profile page UI done

--- components/flechitaregreso.tsx
+++ components/flechitaregreso.tsx
@@ -15,7 +15,7 @@ export default function flechitaregreso ( texto: ruta) {
         router.navigate(`${texto.ruta}`);
     };
     return(
-     <View className="ml-1">
+     <View className="ml-3 mt-2">
                     <IconButton
                         icon={() => (
                         <AntDesign
